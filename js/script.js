6d1b2bf5699ed59e70a19d27e89e32db77abac19
Matt <matt.furlong.12@gmail.com>
2020-10-25 17:39:27 -0400

    completed portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  console.log("clicked");
+  document.getElementById("nav-list-mobile").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("nav-list-mobile").style.width = "0%";
+}
